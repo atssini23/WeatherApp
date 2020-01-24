ca7f5de002eb2eb7a33ec51cfb4786bcbe91b369
Atssini Cabrera <atssini23@yahoo.com>
2020-01-24 14:54:55 -0800

    Initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Wearther Finder
+          <p>Find out temperature, conditions and more...</p>
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Titles;

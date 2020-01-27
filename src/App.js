66dda48225259be8ca63483d35a040a9b59e9f6c
Atssini Cabrera <atssini23@yahoo.com>
2020-01-27 09:02:35 -0800

    minor updates
    updates on api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
+import Weather from "./components/Weather";
 import "./styles.css";
+//import ReactDOM from "react-dom";
+
+const API_KEY = "494f238602ea502b5f5e150131a29438";
 
 export default function App() {
+  getWeather = async () => {
+    const api_call = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=Seattle"
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
+
   return (
     <div className="App">
       <Titles />
       <Form />
+      <Weather />
     </div>
   );
 }
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,10 +4,8 @@ class Titles extends React.Component {
   render() {
     return (
       <div>
-        <h1>
-          Wearther Finder
-          <p>Find out temperature, conditions and more...</p>
-        </h1>
+        <h1>Wearther Finder</h1>
+        <p>Find out temperature, conditions and more...</p>
       </div>
     );
   }

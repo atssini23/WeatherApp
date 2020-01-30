101febe6e99c606885e425f3421e2c59ce16fde6
Atssini Cabrera <atssini23@yahoo.com>
2020-01-30 08:51:03 -0800

    Created Const
    created const over class for speed and performance. Added notes to indicate the other possibilities.

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-class Titles extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Weather Finder</h1>
-        <p>Find out temperature, conditions and more...</p>
-      </div>
-    );
-  }
-}
+//class Titles extends React.Component {
+const Titles = () => (
+  //render() {
+  //return (
+  <div>
+    <h1>Weather Finder</h1>
+    <p>Find out temperature, conditions and more...</p>
+  </div>
+);
+//}
+//}
 
 export default Titles;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,21 +1,23 @@
 import React from "react";
 
-class Weather extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.city && this.props.country && (
-          <p>
-            Location:{this.props.city},{this.props.country}
-          </p>
-        )}
-        {this.props.temperature && <p>Temperature:{this.props.temperature}</p>}
-        {this.props.humidity && <p>Humidity:{this.props.humidity}</p>}
-        {this.props.description && <p>Conditions:{this.props.description}</p>}
-        {this.props.error && <p>{this.props.error}</p>}
-      </div>
-    );
-  }
-}
+//class Weather extends React.Component {
+const Weather = props => (
+  // render() {
+  //return (
+  //since we are only returning one div we dont need any of the return and render
+  <div>
+    {props.city && props.country && (
+      <p>
+        Location:{props.city},{props.country}
+      </p>
+    )}
+    {props.temperature && <p>Temperature:{props.temperature}</p>}
+    {props.humidity && <p>Humidity:{props.humidity}</p>}
+    {props.description && <p>Conditions:{props.description}</p>}
+    {props.error && <p>{props.error}</p>}
+  </div>
+);
+//};
+//}
 
 export default Weather;
